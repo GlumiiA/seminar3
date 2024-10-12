936dfe8ff3aec4764e0a1b412950942ecbfc2a57
Aigul Ibragimova <168688021+GlumiiA@users.noreply.github.com>
2024-10-12 08:28:54 +0300

    Create lib.asm

---
+++ task2/lib.asm
@@ -0,0 +1,34 @@
+section .data
+codes:
+    db      '0123456789ABCDEF'
+
+section .text
+global _start
+
+exit:                        
+    mov  rax, 60           
+    xor  rdi, rdi        
+    syscall                  
+
+print_hex:
+    mov     rdx, 1          
+    mov     rcx, 64         ; Количество бит (8 байт * 8 бит)
+.loop:
+    push    rax             
+    sub     rcx, 4          ; Переходим к следующему 4-битному блоку
+    sar     rdi, cl         ; Сдвигаем число
+    and     rdi, 0xf        
+    lea     rsi, [codes + rdi] ; Находим символ в таблице
+
+    mov     rax, 1          
+    mov     rdi, 1          
+
+    push    rcx             
+    syscall                 
+    pop     rcx             
+
+    pop     rax             
+    test    rcx, rcx       ; Проверяем, закончили ли мы
+    jnz     .loop           ; Повторяем цикл, если не закончили
+
+    ret    
